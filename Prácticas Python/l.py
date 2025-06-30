832a34a2ed9970468ea49831fd80ab1b0365acf6
toxid1gy <dnykyo@gmail.com>
2025-06-29 23:14:12 -0300

    V1.0 Prodigy Store

--- Prácticas Python/l.py
+++ Prácticas Python/l.py
@@ -1,4 +1,4 @@
-class Perro:
-    mi_perro = Perro()
-    print(type(mi_perro))
-    pass
+touch index.html contacto.html cart.html login.html register.html README.md
+touch css/styles.css css/header.css css/hero.css css/productos.css css/contacto.css css/footer.css css/modal.css
+touch js/script.js js/cart.js js/auth.js
+touch data/productos.json
